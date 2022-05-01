3b84047c63fcb3e66883cae3fa905e49571008be
Michael Tirona <tironamichael@gmail.com>
2022-05-01 12:27:42 -0400

    Updated site to include new skills

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import './about.css'
-import picture from './profile.jpg'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import ScrollAnimation from 'react-animate-on-scroll'
+import "./about.css";
+import picture from "./profile.jpg";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
       lineHeight: 1.3,
       marginLeft: theme.spacing(2),
       marginRight: theme.spacing(2),
-      marginBottom: theme.spacing(2)
+      marginBottom: theme.spacing(2),
     },
     lineHeight: 1.2,
     fontFamily: "roboto mono",
@@ -45,7 +45,7 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(14),
     color: "white",
     fontSize: 45,
-    textAlign: "left",
+    textAlign: "center",
     [theme.breakpoints.down("xs")]: {
       fontSize: 23,
       margin: theme.spacing(1),
@@ -63,7 +63,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const About = (props) => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={classes.section}>
@@ -88,15 +88,17 @@ const About = (props) => {
           >
             <Typography className={classes.text} align="center">
               I'm a front end web developer that's fascinated by innovation with
-              a background in customer service and content creation.
-              With a decade's worth of extensive customer service experience at The
-              Walt Disney Company and Apple, I'm a team player that thrives on constructive feedback, communication, and am always willing to learn new things.
+              a background in customer service and content creation. With a
+              decade's worth of extensive customer service experience at The
+              Walt Disney Company and Apple, I'm a team player that thrives on
+              constructive feedback, communication, and am always willing to
+              learn new things.
             </Typography>
             {/* <br /> */}
             <Typography className={classes.text} align="center">
-              Currently based out of Southern California, I'll soon be making
-              the move to Orlando, FL. Open to freelance and full time
-              opportunities, both local and remote. At the moment, I'm creating content as a freelance developer for CodeCast.
+              Based in central Florida. Open to freelance and full time
+              opportunities, both local and remote. At the moment, I'm a
+              Frontend Engineer Intern at Webstacks.
             </Typography>
           </Grid>
         </ScrollAnimation>
@@ -106,6 +108,6 @@ const About = (props) => {
       </Grid>
     </div>
   );
-}
+};
 
-export default About
+export default About;
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { makeStyles } from "@material-ui/core/styles"
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import TwitterIcon from '@material-ui/icons/Twitter'
-import EmailIcon from '@material-ui/icons/Email'
-import { IconButton } from '@material-ui/core'
-import 'animate.css/animate.min.css'
-import ScrollAnimation from 'react-animate-on-scroll'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import EmailIcon from "@material-ui/icons/Email";
+import { IconButton } from "@material-ui/core";
+import "animate.css/animate.min.css";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const useStyles = makeStyles((theme) => ({
   section: {
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "bold",
     [theme.breakpoints.down("xs")]: {
       fontSize: 30,
-    }
+    },
   },
   container: {
     maxWidth: "475px",
@@ -60,86 +60,80 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Contact = () => {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-      <div>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          sm={12}
-        >
-          <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
-            <section id="contact"></section>
-            <Typography className={classes.title} variant="h2" align="center">
-              Let's work together
-            </Typography>
-          </ScrollAnimation>
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          textAlign="center"
-          sm={12}
-        >
-          <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
-            <Paper className={classes.paper}>
-              <div className={classes.contact}>
-                <IconButton
-                  href="mailto:tironamichael@gmail.com"
-                  className={classes.iconBtn}
-                >
-                  <EmailIcon fontSize="large" className={classes.icon} />
-                </IconButton>
-                <IconButton
-                  href="https://twitter.com/tironacodes"
-                  className={classes.iconBtn}
-                  target="_blank"
-                >
-                  <TwitterIcon fontSize="large" className={classes.icon} />
-                </IconButton>
-                <IconButton
-                  href="https://github.com/tironam"
-                  className={classes.iconBtn}
-                  target="_blank"
-                >
-                  <GitHubIcon fontSize="large" className={classes.icon} />
-                </IconButton>
-                <IconButton
-                  href="https://www.linkedin.com/in/michael-tirona-9990a7101/"
-                  className={classes.iconBtn}
-                  target="_blank"
-                >
-                  <LinkedInIcon fontSize="large" className={classes.icon} />
-                </IconButton>
-                <Button
-                  href="https://www.upwork.com/freelancers/~015d0ebc71e45c1972"
-                  className={classes.iconBtn}
-                >
-                  Upwork
-                </Button>
-                <Button
-                  href="https://www.fiverr.com/tironam"
-                  className={classes.iconBtn}
-                >
-                  Fiverr
-                </Button>
-                <Button
-                  href="https://www.codecast.io/users/tironam"
-                  className={classes.iconBtn}
-                >
-                  CodeCast
-                </Button>
-              </div>
-            </Paper>
-          </ScrollAnimation>
-        </Grid>
-      </div>
-    );
-}
+  return (
+    <div>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        sm={12}
+      >
+        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+          <section id="contact"></section>
+          <Typography className={classes.title} variant="h2" align="center">
+            Let's work together
+          </Typography>
+        </ScrollAnimation>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        textAlign="center"
+        sm={12}
+      >
+        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+          <Paper className={classes.paper}>
+            <div className={classes.contact}>
+              <IconButton
+                href="mailto:tironamichael@gmail.com"
+                className={classes.iconBtn}
+              >
+                <EmailIcon fontSize="large" className={classes.icon} />
+              </IconButton>
+              <IconButton
+                href="https://twitter.com/tironacodes"
+                className={classes.iconBtn}
+                target="_blank"
+              >
+                <TwitterIcon fontSize="large" className={classes.icon} />
+              </IconButton>
+              <IconButton
+                href="https://github.com/tironam"
+                className={classes.iconBtn}
+                target="_blank"
+              >
+                <GitHubIcon fontSize="large" className={classes.icon} />
+              </IconButton>
+              <IconButton
+                href="https://www.linkedin.com/in/michael-tirona-9990a7101/"
+                className={classes.iconBtn}
+                target="_blank"
+              >
+                <LinkedInIcon fontSize="large" className={classes.icon} />
+              </IconButton>
+              <Button
+                href="https://www.upwork.com/freelancers/~015d0ebc71e45c1972"
+                className={classes.iconBtn}
+              >
+                Upwork
+              </Button>
+              <Button
+                href="https://www.fiverr.com/tironam"
+                className={classes.iconBtn}
+              >
+                Fiverr
+              </Button>
+            </div>
+          </Paper>
+        </ScrollAnimation>
+      </Grid>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
